b476cdd31f075811d09e5a4e371ec036249cdb6c
Ostabo <osi.borkenhagen@gmail.com>
2024-06-08 20:09:20 +0200

    remove head if mobile (width < 500) + lazy show main landing

--- src/components/MainHeroImage.tsx
+++ src/components/MainHeroImage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import { ThreeJsCanvas } from './Head';
+import useResponsiveSize from '../hooks/useResponsiveSize';
 
 const MainHeroImage = () => {
+  const { width } = useResponsiveSize();
   return (
     <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-      <ThreeJsCanvas></ThreeJsCanvas>
+      {width > 500 ? <ThreeJsCanvas></ThreeJsCanvas> : null}
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,21 +25,23 @@ library.add(faAndroid);
 const App = () => {
   return (
     <div className={`bg-background grid gap-y-16 overflow-hidden`}>
-      <div className={`relative bg-background`}>
-        <div className="max-w-7xl mx-auto">
-          <div
-            className={`relative z-10 pb-8 bg-background sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32`}
-          >
-            <Header />
-            <LazyShow>
-              <>
-                <MainHero />
-              </>
-            </LazyShow>
+      <LazyShow>
+        <div className={`relative bg-background`}>
+          <div className="max-w-7xl mx-auto">
+            <div
+              className={`relative z-10 pb-8 bg-background sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32`}
+            >
+              <Header />
+              <LazyShow>
+                <>
+                  <MainHero />
+                </>
+              </LazyShow>
+            </div>
           </div>
+          <MainHeroImage />
         </div>
-        <MainHeroImage />
-      </div>
+      </LazyShow>
       <AnimatedDivider></AnimatedDivider>
       <LazyShow>
         <>
