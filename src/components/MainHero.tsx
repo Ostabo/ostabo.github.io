d281354f06f1e5ce3ffe478faa1d26f2bfbaaf78
Ostabo <osi.borkenhagen@gmail.com>
2024-06-20 16:39:02 +0200

    adjust name animation

--- src/components/MainHero.tsx
+++ src/components/MainHero.tsx
@@ -11,15 +11,21 @@ const MainHero = () => {
   const { width } = useResponsiveSize();
   const [counter, setCounter] = useState(0);
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCounter((prevCounter) => (prevCounter + 1) % partSubTitles.length);
-    }, 2500);
+    const interval = setInterval(
+      () => {
+        setCounter((prevCounter) => (prevCounter + 1) % partSubTitles.length);
+      },
+      (counter + 1) % partSubTitles.length === 0 ? 5000 : 2500,
+    );
     return () => clearInterval(interval);
-  }, [partSubTitles.length]);
+  }, [counter, partSubTitles.length]);
   return (
     <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
       <div className="sm:text-center lg:text-left">
         <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+          <span className={'absolute opacity-0'}>
+            Oskar Borkenhagen, Ostabo
+          </span>
           <span>{mainHero.title}</span>
           <br />
           <TextTransition
