db0be3d11d59c8b3e0fbe206d693cb496853e343
Ostabo <osi.borkenhagen@gmail.com>
2024-07-17 17:00:01 +0200

    adjust background color + main hero text

--- src/components/MainHeroImage.tsx
+++ src/components/MainHeroImage.tsx
@@ -6,7 +6,12 @@ import useResponsiveSize from '../hooks/useResponsiveSize';
 const MainHeroImage = () => {
   const { width } = useResponsiveSize();
   return (
-    <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+    <div
+      className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2"
+      style={{
+        background: 'linear-gradient(6deg, white, #474fec0d)',
+      }}
+    >
       {width > 500 ? <ThreeJsCanvas></ThreeJsCanvas> : null}
     </div>
   );
